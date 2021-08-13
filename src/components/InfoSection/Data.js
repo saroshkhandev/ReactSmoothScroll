67c4ec1bb084faf2116cdca2afa3aaebf1940f3b
saroshkhandev <saroshabdullah2013@gmail.com>
2021-08-13 21:44:30 +0530

    Created info section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Website",
+  headline: "Get on the Internet with your Website.",
+  description:
+    "A custom-tailored website will help you broaden your reach to the whole world. We all know Smart work > Hard work.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg").default,
+  alt: "Website",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
+import { homeObjOne } from "../components/InfoSection/Data";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar/index";
 
@@ -15,6 +17,7 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggleHandler} />
       <Navbar toggle={toggleHandler} />
       <HeroSection />
+      <InfoSection {...homeObjOne} />
     </>
   );
 };
