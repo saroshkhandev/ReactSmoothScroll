93c1ed7970a2b43257925fc5d87c4921ee8fa569
saroshkhandev <saroshabdullah2013@gmail.com>
2021-08-14 15:32:27 +0530

    Added firebase

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import useInput from "../../hooks/use-input";
 import {
   Container,
   FormWrap,
@@ -13,22 +14,111 @@ import {
   FormTextArea,
 } from "./ContactElements";
 
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
+
 const ContactUS = () => {
+  const {
+    value: enteredFullName,
+    isValid: enteredFullNameIsValid,
+    hasError: fullNameHasError,
+    valueChangeHandler: fullNameChangeHandler,
+    inputBlurHandler: fullNameBlurHandler,
+    reset: resetFullNameInput,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: enteredEmailChangeHandler,
+    inputBlurHandler: enteredEmailBlurHandler,
+    reset: resetEnteredEmailInput,
+  } = useInput(isEmail);
+
+  const {
+    value: enteredMessage,
+    isValid: enteredMessageIsValid,
+    hasError: messageHasError,
+    valueChangeHandler: messageChangeHandler,
+    inputBlurHandler: messageBlurHandler,
+    reset: resetMessageInput,
+  } = useInput(isNotEmpty);
+
+  const errorMessage = <Text>Enter all fields.</Text>;
+
+  let formIsValid = false;
+
+  if (enteredFullNameIsValid && enteredMessageIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    if (!formIsValid) {
+      return;
+    }
+    console.log("Full Name: " + enteredFullName);
+    console.log("Message Name: " + enteredMessage);
+    console.log("Email: " + enteredEmail);
+    const contact = {
+      FullName: enteredFullName,
+      Message: enteredMessage,
+      Email: enteredEmail,
+    };
+    submitMessageHandler(contact);
+    resetFullNameInput();
+    resetMessageInput();
+    resetEnteredEmailInput();
+  };
+
+  const submitMessageHandler = async (message) => {
+    await fetch(
+      "https://react-http-3ae99-default-rtdb.firebaseio.com/messages.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          message: message,
+        }),
+      }
+    );
+  };
+
   return (
     <Container>
       <FormWrap>
         <Icon to="/">TheDopeGuys</Icon>
         <FormContent>
-          <Form action="#">
+          <Form action="#" onSubmit={formSubmissionHandler}>
             <FormH1>Contact Us</FormH1>
             <FormLabel htmlFor="email">Email</FormLabel>
-            <FormInput type="email" required />
+            <FormInput
+              type="email"
+              required
+              onChange={enteredEmailChangeHandler}
+              onBlur={enteredEmailBlurHandler}
+            />
             <FormLabel htmlFor="name">Full Name</FormLabel>
-            <FormInput type="text" required />
+            <FormInput
+              type="text"
+              required
+              onChange={fullNameChangeHandler}
+              onBlur={fullNameBlurHandler}
+            />
             <FormLabel htmlFor="message">Message</FormLabel>
-            <FormTextArea type="text" required />
-            <FormButton type="submit">Send</FormButton>
-            <Text>Enter all fields.</Text>
+            <FormTextArea
+              type="text"
+              required
+              onChange={messageChangeHandler}
+              onBlur={messageBlurHandler}
+            />
+            <FormButton type="submit" disabled={!formIsValid}>
+              Send
+            </FormButton>
+            {fullNameHasError &&
+              emailHasError &&
+              messageHasError &&
+              errorMessage}
           </Form>
         </FormContent>
       </FormWrap>
---
+++ src/hooks/use-input.js
@@ -0,0 +1,56 @@
+import { useReducer } from "react";
+
+const initalInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value };
+  }
+  if (action.type === "RESET") {
+    return { value: "", isTouched: false };
+  }
+
+  return {
+    value: "",
+    isTouched: false,
+  };
+};
+
+const useInput = (validateValue) => {
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initalInputState
+  );
+
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
+
+  const valueChangeHandler = (event) => {
+    dispatch({ type: "INPUT", value: event.target.value });
+  };
+
+  const inputBlurHandler = (event) => {
+    dispatch({ type: "BLUR" });
+  };
+
+  const reset = () => {
+    dispatch({ type: "RESET" });
+  };
+
+  return {
+    value: inputState.value,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
+
+export default useInput;
